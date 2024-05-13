70bc3a4de505825b6a4152e06046c8e2e2cb1ca7
MeisterYM <yannick_mart@yahoo.com>
2024-05-13 16:24:24 -0500

    First commit

---
+++ Math/Math.java
@@ -0,0 +1,12 @@
+class Math {
+    public static void main(String[] args) {
+
+        int number = 41;
+
+        if(number == 42) {
+            System.out.println("THE MEANING OF LIFE.");
+        } else {
+            System.out.println("?");
+        }
+    }
+}
