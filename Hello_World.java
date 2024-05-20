d32307d20fe136309a25943e7fbe209407662291
MeisterYM <yannick_mart@yahoo.com>
2024-05-20 15:13:36 -0500

    Changed hello world and added Loops

--- Hello_World.java
+++ Hello_World.java
@@ -1,5 +1,8 @@
 class Hello_World {
     public static void main(String[] args) {
+
+        //Say hello.
         System.out.println("Hello world, again!");
+        
     }
 }
---
+++ Loops/Loops.java
@@ -0,0 +1,10 @@
+class Loops {
+    public static void main(String[] args) {
+
+        //Count to ten.
+        for(int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
