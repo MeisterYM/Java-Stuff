a910ef9469278f94aab31fcf7da64f10dcfed7d3
MeisterYM <yannick_mart@yahoo.com>
2024-06-09 16:51:52 -0500

    Added Functions

---
+++ Functions/Functions.java
@@ -0,0 +1,22 @@
+class Functions {
+    public static void main(String[] args) {
+
+        //Calling hello function.
+        //Hello();
+
+        //Calling addition function.
+        //plus(2, 2);
+    }
+
+    //Function to say hello.
+    static void Hello() {
+        System.out.println("Java to Earth. Do you read me?");
+    }
+
+    //Function to perform addition.
+    static void plus(int x, int y) {
+        int result = x + y;
+
+        System.out.println(result);
+    }
+}
